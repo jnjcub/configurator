91420fbc028aa66f0627a755a5304f63dc76edcb
Jayan Jacob <jayanjacob@gmail.com>
2015-02-10 02:18:41 -0500

    Config class

--- lib/configurator.rb
+++ lib/configurator.rb
@@ -1,5 +1,21 @@
 require "configurator/version"
 
 module Configurator
-  # Your code goes here...
+  class Config
+    def initialize
+      @@options ||= {}
+    end
+
+    private
+
+    def method_missing(name, *args, &blk)
+      if name.to_s =~ /=$/
+        @@options[$`.to_sym] = args.first
+      elsif @@options.key?(name)
+        @@options[name]
+      else
+        super
+      end
+    end
+  end
 end
