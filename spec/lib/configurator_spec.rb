43fa3d2309095e9116b487525a650f9ffcbb5379
Jayan Jacob <jayandevjacob@gmail.com>
2015-03-15 16:31:13 -0400

    Use a string instead boolean

--- spec/lib/configurator_spec.rb
+++ spec/lib/configurator_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Configurator::Config do
   it "sets and gets values assigned" do
     config = Configurator::Config.new
-    config.my_config = true
-    expect(config.my_config).to eql(true)
+    config.my_config = 'abc'
+    expect(config.my_config).to eql('abc')
   end
 end
